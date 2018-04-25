3fe1df642eef02f18fffadfc0c7118e6c23645f9
chaqin <charles.qin@nokia-sbell.com>
2018-04-25 14:14:26 +0800

    code appsrc context

---
+++ src/appsrc_context.h
@@ -0,0 +1,20 @@
+#ifndef __APPSRC_CONTEXT_H__
+#define __APPSRC_CONTEXT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void* create_context();
+
+void destroy_context(void*);
+
+void need_data (GstElement*, guint, void*);
+
+#ifdef __cplusplus
+};  // extern "C"
+#endif
+
+#endif // __APPSRC_CONTEXT_H__
+
